12645fec67f3c1bde78d1fe6de263fce234ac218
SAKTHI MURUGAN E <sakthimurugan3161@gmail.com>
2026-03-14 20:27:19 +0530

    Time: 7 ms (90.51%), Space: 19.4 MB (70.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        def isAnagram(self, s: str, t: str) -> bool:
+            if len(s) != len(t):
+                return False
+ 
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+ 
+        return s_dict == t_dict
+        
