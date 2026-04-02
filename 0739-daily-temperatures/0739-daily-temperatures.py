8ae562837d3cf753e6ffc22ff0369ad04180e534
SAKTHI MURUGAN E <sakthimurugan3161@gmail.com>
2026-04-02 19:38:02 +0530

    Time: 87 ms (82.15%), Space: 27.8 MB (98.74%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []  # store indices
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                answer[prev] = i - prev
+            stack.append(i)
+
+        return answer
